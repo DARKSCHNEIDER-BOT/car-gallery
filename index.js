c1ebbb1308c86805322faf753a7910e4f51af341
DARKSCHNEIDER <emmandubuisi14@gmail.com>
2024-10-20 19:03:59 +0100

    update

--- index.js
+++ index.js
@@ -1,90 +1,91 @@
-// store all cars globally for filteering
-let allCars = []
+let allCars = [];
+async function fetchCarData() {
 
-// define an asynchronous function to fetch car data from json file/
-const fetchCarData = async () => {
     try {
-        // fetch the json file containing car data
-    const response = await fetch('cars.json')
+        // fetch the JSON file containing car data
+        const response = await fetch("cars.json");
+        // Parse the JSON response into a JS object
+        const data = await response.json()
 
 
+        // Store the array of cars in the global variable
+        // "allCars" for future filtering
+        allCars = data.cars
+        displayCars(allCars)
+        generateFilterBtn(allCars)
+        console.log(data.cars);
 
-    const data = await response.json()
-
-
-    // store the Array of cars in the global variable 'allCars' for future filtering
-    allCars = data.cars
-    
-    // initially display all cars when the page load
-    displayCarCards(allCars)
-    generateFilterButtons(allCars)
-
-    // handle anyerrors that occur during the fetch process
-    }catch(error){
-        // log the error message to thth console
-        console.error('Error fetch car data:', error)
+    } catch (error) {
+        console.error("Error fetch car Data: ", error);
     }
-    
-
 }
 
-// define a function to display the car cards on the webpage.
-const displayCarCards = (cars) => {
-    const carContainer = document.getElementById('carContainer')
+// Define a function to display the car cards on the webpage
+function displayCars(cars) {
+    const carContainer = document.getElementById("carContainer");
 
+    // clear any existing container inside the conatiner
+    carContainer.innerHTML = ""
 
-    carContainer.innerHTML = '';
+    cars.forEach(car => {
+        const carCard = document.createElement("div");
 
-    cars.forEach((car) => {
-
-        // create a new <div> element for each car card
-        const carCard = document.createElement('div')
-
-        // add a css class 'card to the <div>' for styling purpose
-        carCard.classList.add('card')
-
-        // add HTML content to the car card, including an image, name, and model of the car.
+        carCard.classList.add("card")
 
         carCard.innerHTML = `
-        <img src="${car.image}" alt="${car.name} ${car.model}" width="300">
-        <h2>${car.name}</h2>
-        <p>Model: ${car.model}</p>
+            <img src="${car.image}" alt= "${car.name} ${car.model}" width="300">
+            <h2>${car.name}</h2>
+            <p>Model: ${car.model}</p>
         `
-
-        // Append the car card to the car container on the webpage 
         carContainer.appendChild(carCard)
-
     })
+
+    if (cars.length === 0) {
+        carContainer.innerHTML = "No data Found"
+
+    }
 }
 
-// DEFINE A FUNCTION TO GENERATE BUTTONS
-const generateFilterButtons = (cars) => {
-    // get the HTML element where the filter buttons will be place
-    const filterButtonContainer = document.getElementById('filterButtons')
-    
+// function to create filter buttons
 
-    // use map() to create an array of all car names
-    const uniqueNames = [...new Set(cars.map(car => car.name))]
+function generateFilterBtn(cars) {
+    const filterBtnContainer = document.getElementById("filterButtons")
 
-    //loop through the array of unique car names
+    // use map() to create an array of all cars
+    const uniqueNames = [...new Set(cars.map(car => car.name))] // use set to remove duplicates and filter buttons will be placed
+
+
+    // loop through the array of unique car names.
     uniqueNames.forEach(name => {
-        // create a new <button> 
-        const button = document.createElement('button')
+        // create a new button
+        const button = document.createElement("button");
 
-        // set the text of the button of the car
+        // set the text of the button to the car name.
         button.textContent = name
-        // add an event listener to the button to filter the cars when clicked
-        button.addEventListener('click', () => {filterCarsByName(name)})
+        button.addEventListener("click", ()=>{filterCarsByName(name)})
 
-        filterButtonContainer.appendChild(button)
+        filterBtnContainer.appendChild(button)
     })
 }
+
 
 function filterCarsByName(name){
     const filteredCar = allCars.filter(car => car.name === name)
 
-    displayCarCards(filteredCar)
+    displayCars(filteredCar)
 }
 
-// fetch and display car data when the page loads
-window.onload = fetchCarData
+function searchCars(query) {
+    const searchedCars = allCars.filter(car => 
+        car.name.toLowerCase().includes(query.toLowerCase()) ||
+        car.model.toLowerCase().includes(query.toLowerCase())
+    )
+    console.log(searchedCars);
+    displayCars(searchedCars);
+}
+
+document.getElementById("searchInput").addEventListener("input", (event) =>{
+    searchCars(event.target.value)
+    console.log(event.target.value);
+})
+window.onload = fetchCarData()
